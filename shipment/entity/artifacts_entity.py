ca33e2e3375cc6cc5101b94f3e3526c3585a486a
Mehadii-Hassan <mehadi.cse01@gmail.com>
2026-06-20 19:33:26 +0600

    updated requirements

--- shipment/entity/artifacts_entity.py
+++ shipment/entity/artifacts_entity.py
@@ -5,3 +5,8 @@ from dataclasses import dataclass
 class DataIngestionArtifacts:
     train_data_file_path: str
     test_data_file_path: str
+
+@dataclass
+class DataValidationArtifacts:
+    data_drift_report_file_path: str
+    validation_status: bool
